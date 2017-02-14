af3640a55aa2ca1278363b0333361aadf8809632
Ryan Lynn <ryanlynn523@gmail.com>
2017-02-14 12:29:29 -0500

    added scroll action to menu items

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 $(function() {
     $('.slogan').toggle('drop', {direction: 'up'}, 2000);
 });
+function scrollUp(){
+   $('html, body').animate({ scrollTop: $('.masthead').position().top}, 2000);
+}
+function scrollAbout(){
+   $('html, body').animate({ scrollTop: $('.about').position().top - $(".menu").height()}, 2000);
+}
+function scrollWhy(){
+   $('html, body').animate({ scrollTop: $('.why').position().top - $(".menu").height()}, 2000);
+}
