1839bf0aedf630a1bbd629084f0e31ac65ce01d9
Ryan Lynn <ryanlynn523@gmail.com>
2017-03-04 13:36:47 -0500

    revamped stylings for all sections, changed fonts, added background images to sections, on menu item select the styling changes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 $(function() {
     $('.slogan').toggle('drop', {direction: 'up'}, 2000);
-    $('.menu').accordion();
+    $('.testimonial-menu').click(function() {
+      $(this).addClass('menu-select');
+      $('.about-menu').removeClass('menu-select');
+   });
+   $('.about-menu').click(function() {
+     $(this).addClass('menu-select');
+     $('.testimonial-menu').removeClass('menu-select');
+  });
+  $('.fa-chevron-up').click(function() {
+    $('*').removeClass('menu-select');
+  });
 });
 function scrollUp(){
    $('html, body').animate({ scrollTop: $('.masthead').position().top}, 2000);
@@ -8,6 +18,6 @@ function scrollUp(){
 function scrollAbout(){
    $('html, body').animate({ scrollTop: $('.about').position().top - $(".menu").height()}, 2000);
 }
-function scrollWhy(){
-   $('html, body').animate({ scrollTop: $('.why').position().top - $(".menu").height()}, 2000);
+function scrollTestimonial(){
+   $('html, body').animate({ scrollTop: $('.testimonial').position().top - $(".menu").height()}, 2000);
 }
