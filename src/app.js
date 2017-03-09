7a7888897652d6192ac01602717f4cef2c9aa42e
Ryan Lynn <ryanlynn523@gmail.com>
2017-03-09 11:03:55 -0500

    added responsive bar menu

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 $(function() {
     $('.slogan').toggle('drop', {direction: 'up'}, 2000);
-    $('.testimonial-menu').click(function() {
-      $(this).addClass('menu-select');
-      $('.about-menu').removeClass('menu-select');
-   });
    $('.about-menu').click(function() {
      $(this).addClass('menu-select');
      $('.testimonial-menu').removeClass('menu-select');
+     $('.tools-menu').removeClass('menu-select');
+     $('.bar-menu').hide('bar-menu');
   });
+  $('.testimonial-menu').click(function() {
+    $(this).addClass('menu-select');
+    $('.about-menu').removeClass('menu-select');
+    $('.tools-menu').removeClass('menu-select');
+    $('.bar-menu').hide('bar-menu');
+ });
+ $('.tools-menu').click(function() {
+   $(this).addClass('menu-select');
+   $('.about-menu').removeClass('menu-select');
+   $('.testimonial-menu').removeClass('menu-select');
+   $('.bar-menu').hide('bar-menu');
+});
   $('.fa-chevron-up').click(function() {
     $('*').removeClass('menu-select');
+  });
+  $('.fa-bars').click(function() {
+    $('.bar-menu').show();
   });
 });
 function scrollUp(){
@@ -18,6 +31,12 @@ function scrollUp(){
 function scrollAbout(){
    $('html, body').animate({ scrollTop: $('.about').position().top - $(".menu").height()}, 2000);
 }
+function scrollTools(){
+   $('html, body').animate({ scrollTop: $('.tools').position().top - $(".menu").height()}, 2000);
+}
 function scrollTestimonial(){
    $('html, body').animate({ scrollTop: $('.testimonial').position().top - $(".menu").height()}, 2000);
+}
+function scrollContact(){
+   $('html, body').animate({ scrollTop: $('.contact').position().top - $(".menu").height()}, 2000);
 }
