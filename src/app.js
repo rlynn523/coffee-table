b403ce58f5d97bda7b95401d4ed30fa021b883cd
Ryan Lynn <ryanlynn523@gmail.com>
2017-03-10 15:24:51 -0500

    moved click events to js, changed scroll speed

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 $(function() {
   $('.slogan').toggle('drop', {direction: 'up'}, 2000);
   $('.about-menu').click(function() {
+    scrollAbout();
     $(this).addClass('menu-select');
     $('.testimonial-menu').removeClass('menu-select');
     $('.tools-menu').removeClass('menu-select');
     $('.bar-menu').hide('bar-menu');
   });
   $('.testimonial-menu').click(function() {
+    scrollTestimonial();
     $(this).addClass('menu-select');
     $('.about-menu').removeClass('menu-select');
     $('.contact-menu').removeClass('menu-select');
@@ -14,6 +16,7 @@ $(function() {
     $('.bar-menu').hide('bar-menu');
   });
   $('.tools-menu').click(function() {
+    scrollTools();
     $(this).addClass('menu-select');
     $('.about-menu').removeClass('menu-select');
     $('.contact-menu').removeClass('menu-select');
@@ -21,6 +24,7 @@ $(function() {
     $('.bar-menu').hide('bar-menu');
   });
   $('.contact-menu').click(function() {
+    scrollContact();
     $(this).addClass('menu-select');
     $('.about-menu').removeClass('menu-select');
     $('.testimonial-menu').removeClass('menu-select');
@@ -28,6 +32,7 @@ $(function() {
     $('.bar-menu').hide('bar-menu');
   });
   $('.fa-chevron-up').click(function() {
+    scrollUp();
     $('*').removeClass('menu-select');
   });
   $('.fa-bars').click(function() {
@@ -35,17 +40,17 @@ $(function() {
   });
 });
 function scrollUp(){
-   $('html, body').animate({ scrollTop: $('.masthead').position().top}, 2000);
+   $('html, body').animate({ scrollTop: $('.masthead').position().top}, 1500);
 }
 function scrollAbout(){
-   $('html, body').animate({ scrollTop: $('.about').position().top - $(".menu").height()}, 2000);
+   $('html, body').animate({ scrollTop: $('.about').position().top - $(".menu").height()}, 1500);
 }
 function scrollTools(){
-   $('html, body').animate({ scrollTop: $('.tools').position().top - $(".menu").height()}, 2000);
+   $('html, body').animate({ scrollTop: $('.tools').position().top - $(".menu").height()}, 1500);
 }
 function scrollTestimonial(){
-   $('html, body').animate({ scrollTop: $('.testimonial').position().top - $(".menu").height()}, 2000);
+   $('html, body').animate({ scrollTop: $('.testimonial').position().top - $(".menu").height()}, 1500);
 }
 function scrollContact(){
-   $('html, body').animate({ scrollTop: $('.contact').position().top - $(".menu").height()}, 2000);
+   $('html, body').animate({ scrollTop: $('.contact').position().top - $(".menu").height()}, 1500);
 }
